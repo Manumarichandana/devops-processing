54e2279b3df63361274bc2c5b698b6473fbd7710
Manumarichandana <msaichandana213@gmail.com>
2026-04-05 00:15:11 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    description = " region of s3 bucket creation in dev"
+}
+
+variable "bucket" {
+    description = " The name of the bucket"
+}
+
+variable "versioning" {
+    description = " The versioning is on on files"
+}

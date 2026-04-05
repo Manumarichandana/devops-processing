a2d184262ed668680b4ddb572ea14eef38063207
Manumarichandana <msaichandana213@gmail.com>
2026-04-04 22:22:45 -0400

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -2,6 +2,5 @@ module "s3" {
     source  = "git::https://github.com/Manumarichandana/terraform-modules.git//s3?ref=module"
     bucket_name = var.bucket_name
     region  = var.region
-    bucket  = var.bucket
     versioning  = var.versioning
 }
